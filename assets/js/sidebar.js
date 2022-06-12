29b5a1f1123b21b2227678d49a5c8703c79e31ff
Arthur van Kruining <arthurvk@xs4all.nl>
2022-06-12 21:09:12 +0200

    Sidebar toegevoegd en logo veranderd

---
+++ assets/js/sidebar.js
@@ -0,0 +1,24 @@
+// Set the width of the sidebar to 250px and the left margin of the page content to 250px
+// function openNav() {
+//   document.getElementById("mySidebar").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+// } 
+
+document.getElementById("areah").addEventListener("click", function(event){
+  event.preventDefault();
+});
+
+
+// Haal trunk hieronder weg als sidebar trunk moet overlappen ipv wegduwen.
+function toggleNav() {
+    var sidenav = document.getElementById("mySidebar"),
+    trunk = document.getElementById("trunk");
+    sidenav.style.width = sidenav.style.width === "250px" ? '0' : '250px';
+    trunk.style.marginRight = trunk.style.marginRight === "250px" ? '0' :  '250px';
+}
+
+// Set the width of the sidebar to 0 and the left margin of the page content to 0 
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("trunk").style.marginRight = "0";
+}
