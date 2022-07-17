c9e08315602c2dce937d8025a4837323b08eecbf
Arthur van Kruining <arthurvk@xs4all.nl>
2022-07-17 16:07:09 +0200

    Archiefwerk en sidebar-vertraging

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -16,12 +16,16 @@ if (areah !== null) {
 function toggleNav() {
     var sidenav = document.getElementById("mySidebar"),
     band = document.getElementById("band");
-    sidenav.style.width = sidenav.style.width === "fit-content" ? '0' : 'fit-content';
+    setTimeout(function() { 
+      sidenav.style.width = sidenav.style.width === "fit-content" ? '0' : 'fit-content';
+    }, 300 , "ease-out");
     // band.style.marginRight = band.style.marginRight === "250px" ? '0' :  '250px';
 }
 
 // Set the width of the sidebar to 0 and the left margin of the page content to 0 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("band").style.marginRight = "0";
+  setTimeout(function() { 
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("band").style.marginRight = "0";
+  }, 300 , "ease-out");
 }
